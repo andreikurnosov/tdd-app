dff36a869c299e1ade6eff43925c9bf81474e2fd
nixdie <nixdie@yandex.ru>
2020-03-30 23:35:47 +0700

    bugfix

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <el-container>
+  <el-container
+    style="max-width: 50vw; height: 90vh; margin: auto; background-color: #faf9f7;"
+  >
     <el-main>
       <router-view />
     </el-main>
